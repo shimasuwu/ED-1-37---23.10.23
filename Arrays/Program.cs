e4a1da6b75d53624611ede64d59a13f4e5abbb11
Tugberk Gungor <tgbrkgng@hotmail.com>
2023-10-24 01:14:46 +0300

    cok boyutlu diziler

---
+++ Arrays/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] students = new string[3];
+            //students[0] = "Engin";
+            //students[1] = "Derin";
+            //students[2] = "Salih";
+
+
+            //string[] students2 =  {"Engin", "Derin", "Salih"};
+            //students2[2] = "Ahmet";
+
+            ////foreach (string s in students) {
+            ////    Console.WriteLine(s);
+            ////}
+
+            //foreach (string s in students2)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            string[,] regions = new string[5, 3] // satir(5) - sutun(3)
+            {
+                {"Istanbul", "Izmit", "Balikesir"},
+                {"Ankara", "Konya", "Kirikkale"},
+                {"Antalya", "Antalya", "Mersin"},
+                {"Rize", "Trabzon", "Samsun"}, 
+                {"Izmir", "Mugla", "Manisa"}, 
+            };
+
+            for (int i = 0; i <= regions.GetUpperBound(0); i++) //satir
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++) //sutun
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
