51f57152b504209cd02d998178c3b3b07bdab6ed
Tugberk Gungor <tgbrkgng@hotmail.com>
2023-10-23 23:54:37 +0300

    Conditionals

---
+++ Conditionals/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 11;
+
+            //if (number == 10)
+            //{
+            //    Console.WriteLine("number is 10");
+            //}
+            //else if (number ==20)
+            //{
+            //    Console.WriteLine("number is 25");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("number is 10 or 20");
+            //}
+
+            //Console.WriteLine(number==10 ? "number is 10" : "number is not 10");
+
+            //switch (number)
+            //{
+            //    case 10:
+            //        Console.WriteLine("number is 10");
+            //        break;
+
+            //    case 20:
+            //        Console.WriteLine("number is 20");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("number is 10 or 20");
+            //        break;
+            //}
+
+            //if (number>=0 && number <=100)
+            //{
+            //    Console.WriteLine("number is between 0 and 100");
+            //}
+            //else if (number >100 && number<=200)
+            //{
+            //    Console.WriteLine("number is between 101 and 200");
+            //}
+            //else if (number >200 || number <0)
+            //{
+            //    Console.WriteLine("number is greater than 200 or less than 0");
+
+            //}
+
+
+            if (number < 110) //true
+            {
+                if (number >=90&&number<100) //false
+                {
+
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
