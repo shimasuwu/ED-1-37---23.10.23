1d31510e43f0b24b18d304e8fc15a0b33ac650be
Tugberk Gungor <tgbrkgng@hotmail.com>
2023-10-24 22:57:47 +0300

    asal sayiyi bulan program

---
+++ Asal Sayiyi Bulma/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asal_Sayiyi_Bulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (IsPrimeNumber(6))
+            {
+                Console.WriteLine("This is optimus prime");
+            }
+            else { 
+                Console.WriteLine("this is not a prime number");
+            }
+
+            Console.Read();
+
+        }
+        private static bool IsPrimeNumber(int number) {
+
+            bool result = true;
+            for (int i = 2; i < number-1 ; i++)
+            {
+                if (number %2 == 0)
+                {
+                    result = false;
+                }
+            }
+            return result;
+        
+        }
+    }
+}
